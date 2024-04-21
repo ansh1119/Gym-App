2d626b4da427c559e665343e45d2870159a0d9cf
ajeeshrawal <ajeeshrawal3376@gmail.com>
2024-04-22 02:21:19 +0530

    Revert "DueMemberScreen Added"
    This reverts commit 854245fe07ef758e10d555f16b916cd74b2a7de9.

--- app/src/main/java/com/example/gymapp/MainActivity.kt
+++ app/src/main/java/com/example/gymapp/MainActivity.kt
@@ -7,53 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import com.example.gymapp.components.Payment
 
 class MainActivity : ComponentActivity() {
-
-    companion object{
-        var items= listOf(
-            Item(
-                gender = "male",
-                name = "Ajeesh Rawal",
-                number = 7393085376,
-                duration = 3,
-                date = "8/9/2023"
-            ),
-            Item(
-                gender = "male",
-                name = "Ajeesh Rawal",
-                number = 7393085376,
-                duration = 3,
-                date = "8/9/2023"
-            ),
-            Item(
-                gender = "male",
-                name = "Ajeesh Rawal",
-                number = 7393085376,
-                duration = 3,
-                date = "8/9/2023"
-            ),
-            Item(
-                gender = "male",
-                name = "Ajeesh Rawal",
-                number = 7393085376,
-                duration = 3,
-                date = "8/9/2023"
-            ),
-            Item(
-                gender = "male",
-                name = "Ajeesh Rawal",
-                number = 7393085376,
-                duration = 3,
-                date = "8/9/2023"
-            ),
-            Item(
-                gender = "male",
-                name = "Ajeesh Rawal",
-                number = 7393085376,
-                duration = 3,
-                date = "8/9/2023"
-            ),
-        )
-    }
     private val ob: Payment = Payment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
